c314d3e6a489d22a1a8714cc70132c84abfb7806
fas <fas>
2017-03-06 10:08:36 -0600

    Hola mundo en ensamblador

---
+++ hello.asm
@@ -0,0 +1,17 @@
+										;fichero hola.asm
+section .data							;Inicio de la sección datos, para datos inicializados
+	mensaje bd "Hola ensamblador", 0x0A ;El 10 en ensamblador representa el salto de linea o fin de cadena
+	length equ $-mensaje    			;Ayuda a calcular el tamaño de la variable, con la palabra reservada equ
+
+section .text							;Sección de código
+	global _start	 					;Declaración de una variable global
+
+	_start:								;Inicio del código
+	;------Impresión en pantalla---------
+
+	mov eax, 4							;mov registro_general,valor_enviado | mover (nombre registro),(DESCRIPTORES)
+										;Table de descriptores: 4-llama a la funcion sys_write(), 1-referenciaa a la salida estándar stdout, 2-error
+	mov ebx, 1							
+	mov ecx, mensaje 					;Pone la dirección de la variable msg en el registro ebx
+	mov edx, length 					;Pone la longitud del mensaje incluido
+	int 80h								;Interrupción, al lanzarse se ejecuta el código, llama al OS
