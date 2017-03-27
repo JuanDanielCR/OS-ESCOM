6a65c296ad20a89f7a585815114ca701feedb5fe
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-03-26 18:30:26 -0600

    For funcionando

--- ensamblador/for.asm
+++ ensamblador/for.asm
@@ -9,36 +9,37 @@ section .text
 	global _start
 	_start:
 
-	mov eax,4
-	mov ebx,1
-	mov ecx,m1
-	mov edx,tm1
+	mov rax,4
+	mov rbx,1
+	mov rcx,m1
+	mov rdx,tm1
 	int 80h
 
-	mov eax,3
-	mov ebx,2
-	mov ecx,numero
-	mov edx, 1
+	mov rax,3
+	mov rbx,2
+	mov rcx,numero
+	mov rdx, 1
 	int 80h
 
 	;for
-	mov eax, numero
-	sub eax,'0' ; tomar valor decimal
-	mov ecx, 10
+	mov rax, numero
+	sub rax,'0' ; tomar valor decimal
+	mov rcx, 10
 	for:
-		push ecx
-		mul ecx
-		add eax,'0'
-		mov [resultado],eax
+		push rcx
+		mul rcx
+		add rax,'0'
+		mov [resultado],rax
+		
 
-		mov eax, 4
-		mov ebx, 1
-		mov cx, resultado
-		mov edx, 1
+		mov rax, 4
+		mov rbx, 1
+		mov rcx, resultado
+		mov rdx, 1
 		
 		int 80h
-		mov eax, numero
-		pop ecx
+		mov rax, numero
+		pop rcx
 	loop for
 	;fin
 	mov rax,1
