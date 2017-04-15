0a9bad4ab5b27d7c545cad9c6011fdc1ac531d47
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-04-15 17:13:52 -0500

    Comunicación con pipes y ordenamiento en el padre

--- pipes/FIFOClient.c
+++ pipes/FIFOClient.c
@@ -8,16 +8,21 @@ int main(int argc, char const *argv[])
 {
 	FILE *fp;
 	char readBuffer[80];
-
+	int stop = 0;
 	/*Create FIFO*/
 	mknod("myfifo",S_IFIFO|0666,0);
 
-	/*Open FIFO and write to it*/
+	/*Open FIFO and read to it*/
 	while(1){
 		fp = fopen("myfifo","r");
 		fgets(readBuffer, 80, fp);
 		printf("GET: %s\n", readBuffer);
 		fclose(fp);
+		stop++;
+		if(stop == 2){ //receive 2 messages
+			break;
+		}
 	}
+	printf("%s\n", "Sali");
 	return 0;
 }
--- pipes/matrizParalela.c
+++ pipes/matrizParalela.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <linux/stat.h>
 
 void *crearMatriz(int, int, int **);
 void imprimirMatriz(int, int, int **);
 void *multiplicarMatriz(int **, int **, int **,int, int);
-int filas1,columnas1,filas2,columnas2, hijos, anchoRango, status;
+int filas1,columnas1,filas2,columnas2, hijos, anchoRango, status, nbytes;
 
 int main(int argc, char const *argv[])
 {
 	int **A, **B, **C;
 	srand(time(NULL));
-//Creación pipe
+//Creación pipe de los filedescriptors
 	int fd[2]; 
-	int recibi = 0;
-	pipe(fd);
-
+	int n;
+	int finishedPipes = 0;
 //Creación matriz A
 	printf("%s\n", "Matriz A");
 	printf("%s", "No. filas: ");
@@ -48,23 +48,50 @@ int main(int argc, char const *argv[])
 		anchoRango = filas1 / hijos;
 		printf("ancho: %d\n", anchoRango);
 		for(int i = 0; i < hijos; i++){
+			pipe(fd);
 			pid_t pid = fork();
 			if(pid > 0){
 			//padre, junta las matrices
-				//Cerrar escritura
-				close(fd[1]); 
-				while(wait(&status) > 0);
-				read(fd[0], &recibi, sizeof(recibi));
-
+				close(fd[1]);
+			while(wait(&status) > 0);
+			//Leyendo datos del pipe
+				int tuplaRecibida[3];
+				while ((n = read(fd[0], &tuplaRecibida, sizeof(tuplaRecibida))> 0)) { 
+				// Read until it returns 0 (EOF) or -1 (error)
+			 	/*---printf("i: %d j: %d valor: %d \n", tuplaRecibida[0], tuplaRecibida[1],tuplaRecibida[2]);---*/
+			    	C[tuplaRecibida[0]][tuplaRecibida[1]] = tuplaRecibida [2]; 
+				}
+				finishedPipes++;
+				if(finishedPipes == hijos){
+					imprimirMatriz(filas1,columnas2,C);
+				}
 			}else if(pid == 0){
 			//hijo
-				//Cerrar lectura en el hijo
+			//Calculando rago con la formula dada
+				int min = (((i+1)*anchoRango)-anchoRango);
+				int max = ((i+1)*anchoRango)-1;
+			//Validando que el ultimo hijo tome las filas restantes cuando % regrese algo
+				if((i+1) == hijos){				
+					if((filas1 % hijos) > 0){
+						max =(filas1 -1);
+					}
+				}
+				printf("i: %d min: %d max: %d\n", i+1,min,max);
+			//Realizando la multiplicación usando como limites a min, max
+				C = multiplicarMatriz(A,B,C,min,max);
+				/*----imprimirMatriz(filas1,columnas2,C);----*/
+			//Enviando información a través del pipe
 				close(fd[0]);
-				int aux = 666;
-				write(fd[1],&aux, sizeof(recibi));
-				printf("i: %d\n", i+1);
-				//C = multiplicarMatriz(A,B,C,0,filas1); 
-				//imprimirMatriz(filas1,columnas2,C);
+				for(int i = min; i <= max; i++){
+					for(int j = 0; j < columnas2;j++){
+						int tuplaMatriz[3];
+						tuplaMatriz[0] = i;
+						tuplaMatriz[1] = j;
+						tuplaMatriz[2] = C[i][j];
+						write(fd[1],&tuplaMatriz, sizeof(tuplaMatriz));
+					}
+				}
+				close(fd[1]);
 				break; //los hijos ya no siguen el for
 			}else{
 				printf("%s\n","Error procesos");
@@ -96,7 +123,7 @@ void *crearMatriz(int m, int n, int **matriz){
 void imprimirMatriz(int m, int n, int **matriz){
 	for(int i = 0; i < m; i++){
 		for(int j = 0; j < n; j++){
-			printf("%d ",matriz[i][j]);
+			printf("%d  ",matriz[i][j]);
 		}
 		printf("\n");
 	}
@@ -104,7 +131,7 @@ void imprimirMatriz(int m, int n, int **matriz){
 
 void *multiplicarMatriz(int **A, int **B,int **C, int rangoMin, int rangoMax){
 	//Filas de A deben ser iguales a col de B
-	for(int i = rangoMin; i < rangoMax; i++){
+	for(int i = rangoMin; i <= rangoMax; i++){
 		for (int j = 0; j < columnas2; j++){
 			C[i][j] = 0;
 			for(int k = 0; k < filas2; k++){
